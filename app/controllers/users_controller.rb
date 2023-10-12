6c5bc460302ea6e909cbf8e3ba42e6b6fbbd0993
cherukunuri shiva prasad <shivacherukunuri@gmail.com>
2023-10-12 17:59:54 +0530

    added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+    def get 
+
+        render json: "hi this is shiva from render"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/user", to: "users#get"
+
 end
